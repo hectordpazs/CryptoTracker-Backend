0ee3286162481dfa2298aed82693386df3c72b10
hector paz <hectordpazs@gmail.com>
2022-06-03 14:39:15 -0400

    Backend Readyyyy

--- index.js
+++ index.js
@@ -26,6 +26,18 @@ app.use('/api/currencie', require('./routes/currencie'));
 //escuchar peticiones
 const port = process.env.PORT||'3001';
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
 app.listen(port, '0.0.0.0', ()=>{
     console.log(`Servidor corriendo en puerto ${port}`)
 });
+
